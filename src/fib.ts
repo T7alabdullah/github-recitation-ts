7e01fef9ce182e566f44a54c3de903d00d216bef
Filippos <fdounis@andrew.cmu.edu>
2024-09-01 15:35:13 +0300

    Fix plus operation type check in fibonacci function

--- src/fib.ts
+++ src/fib.ts
@@ -1,10 +1,14 @@
 // util function that computes the fibonacci numbers
 module.exports = function fibonacci(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new TypeError('Input must be a valid number');
+  }
+
   if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   }
 
